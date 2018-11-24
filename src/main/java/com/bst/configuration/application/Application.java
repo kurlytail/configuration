3aa1c2bd51cece9b3db74a0e26231956c57b1bf1
ashwin <ashwinkn@hotmail.com>
2018-11-23 23:42:02 -0600

    Added debug configuration

--- src/main/java/com/bst/configuration/application/Application.java
+++ src/main/java/com/bst/configuration/application/Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableConfigServer
+@RefreshScope
 public class Application extends SpringBootServletInitializer {
 	
 	public static void main(final String[] args) {
