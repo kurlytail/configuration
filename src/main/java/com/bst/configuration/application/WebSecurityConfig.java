3639297ce6466258cd277d1b1a5e33230b2f3a98
ashwin <ashwinkn@hotmail.com>
2018-11-25 09:00:29 -0600

    Removed refresh scope from websecurityconfig

--- src/main/java/com/bst/configuration/application/WebSecurityConfig.java
+++ src/main/java/com/bst/configuration/application/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.bst.configuration.application;
 
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,7 +7,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @EnableWebSecurity
 @Configuration
-@RefreshScope
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
