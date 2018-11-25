6a2d53ac2ac2f2856e6d5670ef1a847aa8e31f6b
ashwin <ashwinkn@hotmail.com>
2018-11-25 08:51:42 -0600

    Cleaned up configuration

--- src/main/java/com/bst/configuration/application/WebSecurityConfig.java
+++ src/main/java/com/bst/configuration/application/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.bst.configuration.application;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @EnableWebSecurity
 @Configuration
+@RefreshScope
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
